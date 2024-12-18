c5cd86e9b622ffb085b008b28594d9d3e08f0314
Farooq404 <shkfarooq7766@gmail.com>
2024-12-18 23:27:21 +0530

    Update ap.js
    Added js 
    for a interactive webpage

--- ap.js
+++ ap.js
@@ -1,8 +1,31 @@
-function game (a){
-   for(let i=1; i<6; i++){
-      console.log(i)
-   }
-}
-game(()=>{
-   console.log("hello")
+window.addEventListener('load',()=>{
+    const form=document.querySelector("#new-task-form");
+    const input=document.querySelector("#new-task-input");
+    const list_el =document.querySelector("#tasks")
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        const task=input.value;
+    
+        console.log(input.value)
+        if(!task){
+            alert("Plaease fill out the task");
+            return;
+         };
+        const task_el =document.createElement("div");
+        task_el.classList.add("task");
+
+        const task_content_el = document.createElement("div");
+        // task_content_el.classList.add("content");
+        task_content_el.innerText=task;
+    
+
+        task_el.appendChild(task_content_el);
+    
+        list_el.appendChild(task_el);
+                
+    })
+
+
 })
+
+console.log("Farooq_Shaik")
